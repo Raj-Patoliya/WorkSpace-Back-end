e6f6c8926d48daa7fb86dedfff8bfa7e696f01be
Raj-Patoliya <rjpeace888@gmail.com>
2023-04-04 20:02:58 +0530

    basic organization implemented

--- back_end/settings.py
+++ back_end/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "organization",
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,10 @@ WSGI_APPLICATION = "back_end.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "workSpace",
+        "USER":"root",
+        "PASSWORD":"password",
     }
 }
 
